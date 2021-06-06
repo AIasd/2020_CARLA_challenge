a7e3744839ccea6daad760b250732f91d559644f
AIasd <zhongziyuan1995@gmail.com>
2021-06-06 15:25:56 -0400

    add switch branch scripts

---
+++ switch_to_9_9.sh
@@ -0,0 +1,12 @@
+cd leaderboard
+git checkout 0.9.9
+cd ..
+cd scenario_runner
+cd ..
+git checkout 0.9.9
+cd carla_project
+git checkout 0.9.9
+cd ..
+git checkout 0.9.9
+easy_install ../carla_0994_no_rss/PythonAPI/carla/dist/carla-0.9.9-py3.7-linux-x86_64.egg
+cd ../../../../2020_CARLA_challenge
